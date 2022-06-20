a30f0d7e84aba258637fd8e5d83b76843aa241ee
Ben Tideswell <ben@fishpig.co.uk>
2022-06-20 21:53:13 +0100

    1.0.10

--- Mock/Magento/Wishlist/Helper/Data.php
+++ Mock/Magento/Wishlist/Helper/Data.php
@@ -6,5 +6,11 @@ namespace Magento\Wishlist\Helper;
 
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    
+    /**
+     * @return bool
+     */
+    public function isAllow()
+    {
+        return false;
+    }
 }
